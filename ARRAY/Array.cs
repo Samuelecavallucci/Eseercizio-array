da14021bc73bc4bf1665f6ad0bf753121e4bec01
samuele cavallucci <cavalluccisamuele369@gmail.com>
2021-03-16 11:09:47 +0100

    Aggiungere i file di progetto.

---
+++ ARRAY/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARRAY
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] dati = new int[10];
+
+            //leggere l'array
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(dati[i]);
+            }
+
+            //stampa l'array
+            foreach (var x in dati)
+            {
+                Console.WriteLine(x);
+            }
+
+            //copia i dati di un array in un'altro
+            int[] copia_dati = new int[dati.Length];
+            System.array.copy(dati, copia_dati, dati.Length);
+        }
+
+    }
+}
---
+++ Eseercizio array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eseercizio_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
